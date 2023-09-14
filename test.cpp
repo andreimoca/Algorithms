5382f718d368843492ad48591034ed30dc59ab88
andreimoca <144323110+andreimoca@users.noreply.github.com>
2023-09-14 13:02:27 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,159 @@
+/// Moca Andrei - 100p
+
+#include <bits/stdc++.h>
+using namespace std;
+class InParser {
+private:
+	FILE *fin;
+	char *buff;
+	int sp;
+
+	char read_ch() {
+		++sp;
+		if (sp == 4096) {
+			sp = 0;
+			fread(buff, 1, 4096, fin);
+		}
+		return buff[sp];
+	}
+
+public:
+	InParser(const char* nume) {
+		fin = fopen(nume, "r");
+		buff = new char[4096]();
+		sp = 4095;
+	}
+
+	InParser& operator >> (int &n) {
+		char c;
+		while (!isdigit(c = read_ch()) && c != '-');
+		int sgn = 1;
+		if (c == '-') {
+			n = 0;
+			sgn = -1;
+		} else {
+			n = c - '0';
+		}
+		while (isdigit(c = read_ch())) {
+			n = 10 * n + c - '0';
+		}
+		n *= sgn;
+		return *this;
+	}
+
+	InParser& operator >> (long long &n) {
+		char c;
+		n = 0;
+		while (!isdigit(c = read_ch()) && c != '-');
+		long long sgn = 1;
+		if (c == '-') {
+			n = 0;
+			sgn = -1;
+		} else {
+			n = c - '0';
+		}
+		while (isdigit(c = read_ch())) {
+			n = 10 * n + c - '0';
+		}
+		n *= sgn;
+		return *this;
+	}
+};
+class OutParser {
+private:
+    FILE *fout;
+    char *buff;
+    int sp;
+
+    void write_ch(char ch) {
+        if (sp == 50000) {
+            fwrite(buff, 1, 50000, fout);
+            sp = 0;
+            buff[sp++] = ch;
+        } else {
+            buff[sp++] = ch;
+        }
+    }
+
+
+public:
+    OutParser(const char* name) {
+        fout = fopen(name, "w");
+        buff = new char[50000]();
+        sp = 0;
+    }
+    ~OutParser() {
+        fwrite(buff, 1, sp, fout);
+        fclose(fout);
+    }
+
+    OutParser& operator << (int vu32) {
+        if (vu32 <= 9) {
+            write_ch(vu32 + '0');
+        } else {
+            (*this) << (vu32 / 10);
+            write_ch(vu32 % 10 + '0');
+        }
+        return *this;
+    }
+
+    OutParser& operator << (long long vu64) {
+        if (vu64 <= 9) {
+            write_ch(vu64 + '0');
+        } else {
+            (*this) << (vu64 / 10);
+            write_ch(vu64 % 10 + '0');
+        }
+        return *this;
+    }
+
+    OutParser& operator << (char ch) {
+        write_ch(ch);
+        return *this;
+    }
+    OutParser& operator << (const char *ch) {
+        while (*ch) {
+            write_ch(*ch);
+            ++ch;
+        }
+        return *this;
+    }
+};
+
+unordered_map<int, int> aib;
+int n, q, op, x, y;
+
+inline void Update(int const& pos, int const& val) {
+    for (int i = pos; i <= n; i += i & -i)
+        aib[i] += val;
+}
+
+inline int Query(int const& pos) {
+    int s = 0;
+    for (int i = pos; i >= 1; i -= i & -i)
+        s += aib[i];
+    return s;
+}
+
+int main() {
+    InParser fin("beculete2");
+OutParser fout("beculete2");
+
+
+    fin >> n >> q;
+    while (q--) {
+        fin >> op >> x;
+        if (op == 1) {
+            fin >> y;
+            Update(x, 1);
+            Update(y + 1, -1);
+        }
+        else {
+            if (Query(x) & 1)
+                fout << "A\n";
+            else fout << "S\n";
+        }
+    }
+
+    return 0;
+}
